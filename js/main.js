7d9f722f12cbb90d779be3eef8b0eea9fcb88937
AleksandrDruk <sashamironov25650v@gmail.com>
2022-07-18 02:43:09 +0300

    fucking slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: false, 
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 500,
+        prevArrow: '<img class="slider__arrow slider__arrow-left"src="./img/left-arrow.svg"alt=""/>',
+        nextArrow: '<img class="slider__arrow slider__arrow-right"src="./img/right-arrow.svg"alt=""/>'
+      });
+});
