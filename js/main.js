f1f4db69dbd4d3ed32221450ab2a2171e79e9799
AleksandrDruk <sashamironov25650v@gmail.com>
2022-07-19 16:09:15 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ $(function () {
         dots: true, 
         fade: true,
         autoplay: true,
-        autoplaySpeed: 500,
+        autoplaySpeed: 3000,
         prevArrow: '<img class="slider__arrow slider__arrow-left"src="./img/left-arrow.svg"alt=""/>',
         nextArrow: '<img class="slider__arrow slider__arrow-right"src="./img/right-arrow.svg"alt=""/>'
-      });
+    });
+  
+      $('.burger').click(function () {
+        $('.menu__list').toggleClass('show-menu')
+    })
 });
